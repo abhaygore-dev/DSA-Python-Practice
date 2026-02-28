491a013d5aa676cb6b13dacc94c661923351407f
Abhay <abhaygore555@gmail.com>
2026-03-01 00:21:29 +0530

    Solved Two Sum - Brute Force version

---
+++ Arrays/01_two_Sum.py
@@ -0,0 +1,15 @@
+# Two Sum - My Solution for the problem 
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        n = len(nums)
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# testing on local pc
+s = Solution()
+print(s.twoSum([2, 7, 11, 15], 9))
